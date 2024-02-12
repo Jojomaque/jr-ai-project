6b646ddf02b3fa29061f6e33445c1513604a6c5c
Jojomaque <81266816+Jojomaque@users.noreply.github.com>
2024-02-12 12:02:10 +0000

    Fixed typo error

--- src/index.js
+++ src/index.js
@@ -11,18 +11,19 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#user-instructions");
-  let apiKey = "01t385ae4o4576bbb84530da05b3d38f";
+
 
   let prompt = `user instructions: Generate a French poem about ${instructionsInput.value}`;
   let context =
     "You are a romantic poem expert and love to write short poems, your mission is to generate a 4 line poem in basic html. Make sure to follow the user instructions.";
+      let apiKey = "01t385ae4o4576bbb84530da05b3d38f";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">❤️ Generating a French poem about ${instructionsInput.value}</div>`;
 
-  axios.get(apiURL).then(displayPoem);
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
